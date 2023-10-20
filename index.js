80368708626f90d8ac9f744083927c04e299cbdf
David Ortiz <david.ortiz@deployedservices.com>
2023-10-20 18:16:40 -0400

    switch cases done

--- index.js
+++ index.js
@@ -7,29 +7,74 @@ console.log(
 		button.addEventListener('click', (e) => {
 			e.preventDefault();
 			const expr = e.target.getAttribute('id');
+			const keyType = e.target.getAttribute('data-type');
 			switch (expr) {
 				case 'start-input-script':
-					appendKeys(expr);
+					appendKeys(expr, keyType);
 					break;
 				case 'send-input':
-					sendInput(expr);
+					sendInput(expr, keyType);
 					break;
 				case 'variable':
 					console.log('this is the variable button');
 					break;
 				case 'input-box':
-					showInputBox(expr);
+					showInputBox(expr, keyType);
 					break;
 				case 'tab':
-					send(expr, 'Tab');
+					send(expr, 'Tab', keyType);
+					break;
+				case 'enter':
+					send(expr, 'Enter', keyType);
+					break;
+				case 'full-date':
+					fullDate();
+					break;
+				case 'month-name':
+					fullMonth();
+					break;
+				case 'current-time':
+					currentTime();
+					break;
+				case 'arrow-up':
+					send(expr, 'Up', keyType);
+					break;
+				case 'arrow-left':
+					send(expr, 'Left', keyType);
+					break;
+				case 'arrow-right':
+					send(expr, 'Right', keyType);
+					break;
+				case 'arrow-down':
+					send(expr, 'Down', keyType);
+					break;
+				case 'exclamation':
+					send(expr, '!', keyType);
+					break;
+				case 'sleep':
+					sendString(`\n Sleep, 300 \n`);
+					break;
+				case 'return':
+					sendString(`\n return \n`);
+					break;
+				case 'new-line':
+					sendString(`\n`);
+					break;
 			}
 		});
 	})
 );
 
-function appendKeys(expr) {
+function sendString(str) {
+	return str;
+}
+
+function appendKeys(expr, type) {
 	if (inputEl.value == '') {
 		return alert('the input field is empty');
+	}
+	if (scriptEl.value != '') {
+		return alert('do not put that there, it goes at the beginning, please');
 	}
 	scriptEl.value += `:R*?:${inputEl.value}::\n`;
 	inputEl.value = '';
@@ -37,7 +82,7 @@ function appendKeys(expr) {
 	return;
 }
 
-function sendInput(expr) {
+function sendInput(expr, type) {
 	if (inputEl.value == '') {
 		return alert('the input field is empty');
 	}
@@ -48,9 +93,9 @@ function sendInput(expr) {
 	return;
 }
 
-function send(expr, str) {
+function send(expr, str, type) {
 	let prefix = 'send, ';
-	if (lastPressed == expr) {
+	if (lastPressed == expr || type == 'key') {
 		console.log(expr.parentElement);
 		scriptEl.value += `{${str}}`;
 		inputEl.value = '';
